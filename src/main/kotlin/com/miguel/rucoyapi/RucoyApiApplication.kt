da12a8d98bfb4392f2a0037e211f13048c9c385d
Miguel Jerónimo <mjeronimo@frd.org.mx>
2023-10-16 12:35:28 -0600

    first commit

---
+++ src/main/kotlin/com/miguel/rucoyapi/RucoyApiApplication.kt
@@ -0,0 +1,11 @@
+package com.miguel.rucoyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RucoyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RucoyApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/miguel/rucoyapi/RucoyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miguel.rucoyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RucoyApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
